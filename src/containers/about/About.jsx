ea810c65da2992d1316144f4cde8ade6c25428a7
Joe Witzke <joewitzke@Joes-MacBook-Pro.local>
2021-11-30 11:41:41 -0500

    completes folder and file structure

---
+++ src/containers/about/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './about.styles.css'
+
+function About() {
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    )
+}
+
+export default About
---
+++ src/containers/projects/Projects.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './projects.styles.css'
+
+function Projects() {
+    return (
+        <div>
+            <h1>Projects Container</h1>
+        </div>
+    )
+}
+
+export default Projects
